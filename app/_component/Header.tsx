58fc059644ba14c08b8990e7176810fa876051de
M40 <modev.404@gmail.com>
2024-03-07 14:28:48 +0300

    Header created

---
+++ app/_component/Header.tsx
@@ -0,0 +1,30 @@
+import { GithubIcon } from 'lucide-react'
+import Link from 'next/link'
+import React from 'react'
+const Header = () => {
+  return (
+  <header className="bg-gray-500 p-4 text-white">
+    <div className="flex items-center justify-between">
+      <div className="flex items-center space-x-4">
+        {/* <img
+          src="path/to/your/logo.png"
+          alt="Logo"
+          className="h-8 w-8 rounded-full"
+        /> */}
+        <span className="text-2xl font-black">MA</span>
+      </div>
+      <Link
+        href="https://github.com/your-username"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex items-center space-x-2"
+      >
+        <GithubIcon size={30} className="text-2xl" />
+        <span>Github</span>
+      </Link>
+    </div>
+  </header>
+  )
+}
+
+export default Header
