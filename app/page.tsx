db142172103b056e17bff4b3d99c2d540e24ca36
M40 <modev.404@gmail.com>
2025-05-07 20:01:30 +0300

    fix and update

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,20 @@
-"use client"
-import Footer from './_component/Footer'
-import Header from './_component/Header'
-import HomeHero from './_component/HomeHero'
-import Projects from './_component/Projects'
-import Skills from './_component/Skills'
-
-export const dynamic = 'force-dynamic';
+"use client";
+import Footer from "./_component/Footer";
+import Header from "./_component/Header";
+import HomeHero from "./_component/HomeHero";
+import Projects from "./_component/Projects";
+import Skills from "./_component/Skills";
 
 const page = () => {
   return (
-    <div className='space-y-4'>
-      <Header/>
-      <HomeHero/>
-      <Skills/>
-      <Projects/>
-      <Footer/>
+    <div className="space-y-4">
+      <Header />
+      <HomeHero />
+      <Skills />
+      <Projects />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
